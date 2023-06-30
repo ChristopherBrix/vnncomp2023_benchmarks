02e0acf4b174cbdcae36c601b8fa4be4c81be697
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2023-06-30 13:21:47 +0200

    Fix for missing vnnlib or onnx directories

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 wget https://rwth-aachen.sciebo.de/s/Erh54iS6fT3hHCr/download -O large_models.zip
 unzip large_models.zip -d large_models
+
+echo "Moving large benchmark files"
 cd large_models/vnncomp2023/
 for d in *
 do
-    echo $d
     cd $d/seed_676744409/;
-    pwd; ls;
+    mkdir -p ../../../../benchmarks/$d/onnx
+    mkdir -p ../../../../benchmarks/$d/vnnlib
     find . -type f -exec mv "{}" "../../../../benchmarks/$d/{}" \;
     cd ../../;
-    echo
 done
 cd ../..
 rm -r large_models large_models.zip
 
+echo "Unzipping"
 gunzip benchmarks/*/onnx/* benchmarks/*/vnnlib/*
